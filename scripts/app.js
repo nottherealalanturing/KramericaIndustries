299dd29119c082e531b42c1de4c9e940fea4ecd4
Assad Isah <nottherealalanturing@gmail.com>
2022-03-08 17:48:11 +0100

    feat: add markup for hero section

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 const navIconMobile = document.getElementById('nav-icon');
+const navMobile = document.querySelector('.navigation-mobile');
 
 navIconMobile.addEventListener('click', () => {
   navIconMobile.classList.toggle('open');
+  navMobile.classList.toggle('navigation-mobile-open');
 });
