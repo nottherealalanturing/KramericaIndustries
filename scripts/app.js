b7fe0a68dec0a8aa1fcebd7a956d75b4062976d8
Assad Isah <nottherealalanturing@gmail.com>
2022-03-10 08:53:39 +0100

    feat: add styles and markup to the about page

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,23 @@
 const navIconMobile = document.getElementById('nav-icon');
 const navMobile = document.querySelector('.navigation-mobile');
+const hero = document.querySelector('.hero');
+const services = document.querySelector('.services');
+const employees = document.querySelector('.employees');
+const footer = document.querySelector('.footer');
 
 navIconMobile.addEventListener('click', () => {
   navIconMobile.classList.toggle('open');
   navMobile.classList.toggle('navigation-mobile-open');
+  services.style.display !== 'none'
+    ? (services.style.display = 'none')
+    : (services.style.display = 'flex');
+  employees.style.display !== 'none'
+    ? (employees.style.display = 'none')
+    : (employees.style.display = 'flex');
+  footer.style.display !== 'none'
+    ? (footer.style.display = 'none')
+    : (footer.style.display = 'flex');
+  hero.style.display !== 'none'
+    ? (hero.style.display = 'none')
+    : (hero.style.display = 'flex');
 });
